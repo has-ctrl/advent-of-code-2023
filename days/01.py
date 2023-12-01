fd0967481a4ec7bee108a37fe3cac943fb3675d2
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-01 13:56:41 +0100

    Day 01 - Part 2

--- days/01.py
+++ days/01.py
@@ -2,6 +2,17 @@ from get_day_input import get_input
 
 
 data = get_input(day=1).splitlines()
+spelled_digits = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
 
 
 def one() -> int:
@@ -19,7 +30,13 @@ def two() -> int:
     """
     What is the sum of all of the calibration values?
     """
-    pass
+    total = 0
+    for text in data:
+        for word, digit in spelled_digits.items():
+            text = text.replace(word, word + str(digit) + word)
+        digits = [s for s in text if s.isdigit()]
+        total += int(digits[0] + digits[-1])
+    return total
 
 
 print(f"1. {one()}")
