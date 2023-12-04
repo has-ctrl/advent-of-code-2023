dc9c3d05e08e1a8946efd690f58969ef3190693d
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-04 23:34:16 +0100

    Day 04 - Part 2

--- days/04.py
+++ days/04.py
@@ -4,16 +4,20 @@ from get_day_input import get_input
 data = get_input(day=4).splitlines()
 
 
+def _find_match(card: str) -> set:
+    w, m = card.split(":")[1].split("|")
+    winning_numbers = set(w.strip().split())
+    my_numbers = set(m.strip().split())
+    return winning_numbers & my_numbers
+
+
 def one() -> int:
     """
     Take a seat in the large pile of colorful cards. How many points are they worth in total?
     """
     total = 0
     for card in data:
-        w, m = card.split(":")[1].split("|")
-        winning_numbers = set(w.strip().split())
-        my_numbers = set(m.strip().split())
-        match = winning_numbers & my_numbers
+        match = _find_match(card)
         if match:
             total += 2 ** (len(match)-1)
     return total
@@ -23,7 +27,14 @@ def two() -> int:
     """
     Including the original set of scratchcards, how many total scratchcards do you end up with?
     """
-    pass
+    copies = {n: 1 for n, _ in enumerate(data)}
+    for i, card in enumerate(data):
+        match = _find_match(card)
+        if match:
+            for j, _ in enumerate(match):
+                copies[i + j + 1] += copies[i]
+    return sum(copies.values())
 
 
 print(f"1. {one()}")
+print(f"2. {two()}")
