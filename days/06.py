7b8bd0abfee710ffc60f3049769a70d2d63e8bad
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-07 23:22:58 +0100

    Day 06 - Part 2

--- days/06.py
+++ days/06.py
@@ -1,7 +1,16 @@
+import math
+
 from get_day_input import get_input
 
 
 data = get_input(day=6).splitlines()
+
+
+def _find_intercepts(a: int, b: int, c: int) -> (int, int):
+    discriminant = b ** 2 - 4 * a * c
+    root1 = (-b + math.sqrt(discriminant)) / 2 * a
+    root2 = (-b - math.sqrt(discriminant)) / 2 * a
+    return int(root1), int(root2)
 
 
 def one() -> int:
@@ -24,10 +33,12 @@ def one() -> int:
 
 def two() -> int:
     """
-    Consider all of the initial seed numbers listed in the ranges on the first line of the almanac.
-    What is the lowest location number that corresponds to any of the initial seed numbers?
+    How many ways can you beat the record in this one much longer race?
     """
-    return 0
+    t = int(data[0].split(":")[1].replace(" ", ""))
+    d = int(data[1].split(":")[1].replace(" ", ""))
+    max_val, min_val = _find_intercepts(1, -t, d)
+    return max_val - min_val
 
 
 print(f"1. {one()}")
