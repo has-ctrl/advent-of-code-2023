7d0b4217496ae6f7a0e734d39b13783a18b45a92
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-08 23:36:24 +0100

    Day 07 - Part 2

--- days/07.py
+++ days/07.py
@@ -4,49 +4,64 @@ from get_day_input import get_input
 
 
 data = get_input(day=7).splitlines()
-strengths = {
-    "A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2
-}
+
+
+def _calculate_hand_type(hand_list: list[tuple[str, int]]) -> int:
+    if hand_list[0][1] == 5:
+        return 6  # Five of a Kind
+    elif hand_list[0][1] == 4:
+        return 5  # Four of a kind
+    elif hand_list[0][1] == 3 and hand_list[1][1] == 2:
+        return 4  # Full House
+    elif hand_list[0][1] == 3:
+        return 3  # Three of a Kind
+    elif hand_list[0][1] == 2 and hand_list[1][1] == 2:
+        return 2  # Two Pair
+    elif hand_list[0][1] == 2:
+        return 1  # One Pair
+    else:
+        return 0  # High Card
 
 
 def one() -> int:
     """
     Find the rank of every hand in your set. What are the total winnings?
     """
+    strengths = {
+        "A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2
+    }
     scores = []
     for row in data:
         hand, bid = row.split()
-        counter = Counter(hand).most_common()
-
-        if counter[0][1] == 5:
-            hand_type = 6  # Five of a Kind
-        elif counter[0][1] == 4:
-            hand_type = 5  # Four of a kind
-        elif counter[0][1] == 3 and counter[1][1] == 2:
-            hand_type = 4  # Full House
-        elif counter[0][1] == 3:
-            hand_type = 3  # Three of a Kind
-        elif counter[0][1] == 2 and counter[1][1] == 2:
-            hand_type = 2  # Two Pair
-        elif counter[0][1] == 2:
-            hand_type = 1  # One Pair
-        else:
-            hand_type = 0  # High Card
-
+        hand_list = Counter(hand).most_common()
+        hand_type = _calculate_hand_type(hand_list)
         score_order = [hand_type, *[strengths[i] for i in hand]]
         scores.append((score_order, bid))
-
-    total = 0
-    for i, (_, bid) in enumerate(sorted(scores)):
-        total += (i + 1) * int(bid)
-    return total
+    return sum([(i + 1) * int(bid) for i, (_, bid) in enumerate(sorted(scores))])
 
 
 def two() -> int:
     """
-    How many ways can you beat the record in this one much longer race?
+    Using the new joker rule, find the rank of every hand in your set. What are the new total winnings?
     """
-    return 0
+    strengths = {
+        "A": 14, "K": 13, "Q": 12, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "J": 1
+    }
+    scores = []
+    for row in data:
+        hand, bid = row.split()
+        hand_list = Counter(hand).most_common()
+        if "J" not in hand:
+            hand_type = _calculate_hand_type(hand_list)
+        else:
+            hand_type = 0
+            for k, v in hand_list:
+                hand_list_copy = Counter(hand.replace('J', k)).most_common()
+                new_hand_type = _calculate_hand_type(hand_list_copy)
+                hand_type = new_hand_type if new_hand_type > hand_type else hand_type
+        score_order = [hand_type, *[strengths[i] for i in hand]]
+        scores.append((score_order, bid))
+    return sum([(i + 1) * int(bid) for i, (_, bid) in enumerate(sorted(scores))])
 
 
 print(f"1. {one()}")
