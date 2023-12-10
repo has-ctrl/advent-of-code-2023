6be207c8c5c6acc3ce06427ca9b471a965d227ff
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-10 21:11:06 +0100

    Day 09 - Part 1

---
+++ days/09.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from get_day_input import get_input
+
+
+data = get_input(day=9).splitlines()
+
+
+def _calc_sequence(values: list[int]) -> int:
+    total = 0
+    while True:
+        if not any(values):
+            return total
+        total += values[-1]
+        values = np.diff(values)
+
+
+def one() -> int:
+    """
+    Analyze your OASIS report and extrapolate the next value for each history. What is the sum of these extrapolated values?
+    """
+    total = 0
+    for history in data:
+        values = [int(v) for v in history.split()]
+        total += _calc_sequence(values)
+    return total
+
+
+def two() -> int:
+    """
+    """
+    return 0
+
+
+print(f"1. {one()}")
+print(f"2. {two()}")
