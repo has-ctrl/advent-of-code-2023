21177d9be30f2cb2280a1380fd1e9b10256bc97e
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-17 00:56:26 +0100

    Day 14 - Part 2

--- days/14.py
+++ days/14.py
@@ -4,14 +4,9 @@ from get_day_input import get_input
 data = get_input(day=14).splitlines()
 
 
-def one() -> int:
-    """
-    Tilt the platform so that the rounded rocks all roll north.
-    Afterward, what is the total load on the north support beams?
-    """
-    transposed_data = [''.join(i) for i in zip(*data)]
+def _calculate_load_dynamic(platform: list[str]) -> int:
     total = 0
-    for vertical in transposed_data:
+    for vertical in platform:
         increment = 0
         for i, c in enumerate(vertical):
             load = len(vertical) - i
@@ -25,11 +20,54 @@ def one() -> int:
     return total
 
 
+def one() -> int:
+    """
+    Tilt the platform so that the rounded rocks all roll north.
+    Afterward, what is the total load on the north support beams?
+    """
+    transposed_data = [''.join(i) for i in zip(*data)]
+    return _calculate_load_dynamic(transposed_data)
+
+
+def _roll(vertical: str) -> str:
+    result = ""
+    for chunk in vertical.split("#"):
+        result += "".join(sorted(chunk, reverse=True)) + "#"
+    return result[:-1]
+
+
+def _tilt(platform: list[str]) -> list[str]:
+    tilted_platform = [''.join(i) for i in zip(*reversed(platform))]
+    return [_roll(v) for v in tilted_platform]
+
+
+def _calculate_load_static(platform: list[str]) -> int:
+    total = 0
+    for vertical in platform:
+        for i, c in enumerate(vertical):
+            load = len(vertical) - i
+            if c == "O":
+                total += load
+    return total
+
+
 def two() -> int:
     """
     Run the spin cycle for 1000000000 cycles. Afterward, what is the total load on the north support beams?
     """
-    return 0
+    platform = list(reversed(["".join(reversed(i)) for i in data]))
+    platforms_seen = [platform]
+    total_cycles = 1_000_000
+    for cycle in range(total_cycles):
+        for _ in range(4):
+            platform = _tilt(platform)
+        if platform in platforms_seen:
+            loop_start_idx = platforms_seen.index(platform)
+            loop_length = cycle - loop_start_idx + 1
+            final_idx = (total_cycles - loop_start_idx) % loop_length + loop_start_idx
+            north_turned_platform = [''.join(i) for i in zip(*reversed(platforms_seen[final_idx]))]
+            return _calculate_load_static(north_turned_platform)
+        platforms_seen.append(platform)
 
 
 print(f"1. {one()}")
