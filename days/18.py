ffacfe858bebbf039320f57f323b71b5cfce1199
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-19 09:11:02 +0100

    Day 18 - Part 2

--- days/18.py
+++ days/18.py
@@ -7,13 +7,10 @@ from get_day_input import get_input
 data = get_input(day=18).splitlines()
 
 
-def _find_polygon_coords(outer_coords: list[tuple[int, int]]) -> int:
+def _find_polygon_coords(outer_coords: list[tuple[int, int]], n_coords: int) -> int:
     p = Polygon(outer_coords)
-    x_min, y_min, x_max, y_max = p.bounds
-    x = np.arange(np.floor(x_min), np.ceil(x_max) + 1)
-    y = np.arange(np.floor(y_min), np.ceil(y_max) + 1)
-    points = MultiPoint(np.transpose([np.tile(x, len(y)), np.repeat(y, len(x))]))
-    return len([(pt.x, pt.y) for pt in points.intersection(p).geoms])
+    # Shoelace formula and Pick's Theorem... Thanks Google.
+    return int(abs(p.area) - 0.5 * n_coords + 1 + n_coords)
 
 
 def one() -> int:
@@ -23,25 +20,45 @@ def one() -> int:
     """
     coord = (0, 0)
     visited = [coord]
+    n_coords = 1
     for command in data:
         direction, amount, color = command.split()
-        for i in range(int(amount)):
-            if direction == "L":
-                coord = coord[0] - 1, coord[1]
-            elif direction == "R":
-                coord = coord[0] + 1, coord[1]
-            elif direction == "U":
-                coord = coord[0], coord[1] - 1
-            elif direction == "D":
-                coord = coord[0], coord[1] + 1
-            visited.append(coord)
-    return _find_polygon_coords(visited)
+        if direction == "L":
+            coord = coord[0] - int(amount), coord[1]
+        elif direction == "R":
+            coord = coord[0] + int(amount), coord[1]
+        elif direction == "U":
+            coord = coord[0], coord[1] - int(amount)
+        elif direction == "D":
+            coord = coord[0], coord[1] + int(amount)
+        visited.append(coord)
+        n_coords += int(amount)
+    return _find_polygon_coords(visited, n_coords)
 
 
 def two() -> int:
     """
+    Convert the hexadecimal color codes into the correct instructions; if the Elves follow this new dig plan,
+    how many cubic meters of lava could the lagoon hold?
     """
-    pass
+    coord = (0, 0)
+    visited = [coord]
+    n_coords = 1
+    for command in data:
+        _, _, color = command.split()
+        direction = color[-2]
+        amount = int(color[2:-2], 16)
+        if direction == "2":
+            coord = coord[0] - amount, coord[1]
+        elif direction == "0":
+            coord = coord[0] + amount, coord[1]
+        elif direction == "3":
+            coord = coord[0], coord[1] - amount
+        elif direction == "1":
+            coord = coord[0], coord[1] + amount
+        visited.append(coord)
+        n_coords += int(amount)
+    return _find_polygon_coords(visited, n_coords)
 
 
 print(f"1. {one()}")
