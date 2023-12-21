755324b707a3e28c9ab1934b27a2d8f4477408b6
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2023-12-21 21:17:27 +0100

    Day 11 - Part 2

--- days/11.py
+++ days/11.py
@@ -17,12 +17,16 @@ def _calc_distance(g1: tuple[int, int], g2: tuple[int, int]) -> int:
     return abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
 
 
-def _expand_universe() -> list[str]:
+def _find_empty_rows_and_columns() -> tuple[list[int], list[int]]:
     galaxy_coordinates = _find_galaxies(data)
     all_rows, all_cols = list(range(len(data[0]))), list(range(len(data)))
     empty_rows = [i for i in all_rows if i not in [t[0] for t in galaxy_coordinates]]
     empty_cols = [j for j in all_cols if j not in [t[1] for t in galaxy_coordinates]]
+    return empty_rows, empty_cols
 
+
+def _expand_universe() -> list[str]:
+    empty_rows, empty_cols = _find_empty_rows_and_columns()
     expanded_universe = data[:]
     for i, r_idx in enumerate(empty_rows):
         expanded_universe.insert(r_idx + i, "." * len(data))
@@ -48,8 +52,25 @@ def one() -> int:
 
 def two() -> int:
     """
+    Starting with the same initial image, expand the universe according to these new rules,
+    then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
     """
-    return 0
+    total = 0
+    multiplier = 1_000_000 - 1
+    galaxies = _find_galaxies(data)
+    empty_rows, empty_cols = _find_empty_rows_and_columns()
+    galaxy_pairs = [(g1, g2) for idx, g1 in enumerate(galaxies) for g2 in galaxies[idx + 1:]]
+    for g1, g2 in galaxy_pairs:
+        r_1, r_2, r_count = g1[0], g2[0], 0
+        c_1, c_2, c_count = g1[1], g2[1], 0
+        for empty_row in empty_rows:
+            if r_1 > empty_row >= r_2 or r_2 > empty_row > r_1:
+                r_count += 1
+        for empty_col in empty_cols:
+            if c_1 > empty_col > c_2 or c_2 > empty_col > c_1:
+                c_count += 1
+        total += _calc_distance(g1, g2) + r_count * multiplier + c_count * multiplier
+    return total
 
 
 print(f"1. {one()}")
